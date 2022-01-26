62e2e6ad55771eb9739cbd505e88c7f58ae6d542
Adenuga Oluwatunmise Wilson <oluwatunmiseadenuga@gmail.com>
2022-01-26 15:47:29 +0100

    chore: update folder structure and setup react query

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { QueryClientProvider, QueryClient } from 'react-query';
+import { ReactQueryDevtools } from "react-query/devtools";
 
-function App() {
+
+
+// Create a client
+const queryClient = new QueryClient()
+
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // Provide the client to your App
+    <QueryClientProvider client={queryClient}>
+      <main>
+        <h1>Hello world</h1>
+      </main>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 }
 
